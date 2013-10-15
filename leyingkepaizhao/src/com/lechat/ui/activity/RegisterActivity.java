bdf26bebc289570e8b620803b62dbed5684ce815
zoushuai <zoushuai_201314@126.com>
2013-10-15 16:04:35 +0800

    no message

---
+++ leyingkepaizhao/src/com/lechat/ui/activity/RegisterActivity.java
@@ -0,0 +1,40 @@
+package com.lechat.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends BaseActivity {
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void click(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void create(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
